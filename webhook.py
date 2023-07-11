b8a90001a1661c0f4da181747b965616738f3b3d
bdyson556 <71849300+bdyson556@users.noreply.github.com>
2023-07-10 22:32:44 -0500

    Able to parse user request. Having difficulty connecting to DynamoDB.

--- webhook.py
+++ webhook.py
@@ -6,21 +6,38 @@ import os
 from flask import Flask, request, abort
 from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
+import boto3
+
 
 app = Flask(__name__)
+dynamo_client = boto3.resource("dynamodb")
+dynamo_table = None
 
 @app.route('/', methods=['POST'])
 def reset():
     validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
-    if not validator.validate(
+    try:
+        if not validator.validate(
             request.url,
             request.form,
             request.headers.get('X-Twilio-Signature')
-    ):
-        abort(400)
+        ):
+            print("Failed to authenticate")
+            abort(400)
+    except Exception as e:
+        print(f"Authentication error: {str(e)}")
+
     message_body = request.form["Body"]
+    try:
+        if message_body.lower() == "lockbox":
+            dynamo_table = "key-codes"
+            code_id = "uptown-condo-front-lockbox"
+        code = dynamo_table.get_item(Key={'code-id': code_id}).get("Item")
+    except Exception as e:
+        print(f"Authentication error: {str(e)}")
+        abort(400)
     resp = MessagingResponse()
-    resp.message(f"This is literally what you just texted: {message_body}")
+    resp.message(f"Your code is: {code}")
     return str(resp), 200, {'Content-Type': 'application/xml'}
 
 
